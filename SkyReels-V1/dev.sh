4d5404b7e59d956ce2ab7b764873e76c4ddfcd6c
sobermh <409788696@qq.com>
2025-04-17 11:19:11 +0800

    refactor: refactor annotation code

--- SkyReels-V1/dev.sh
+++ SkyReels-V1/dev.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+########################################
+# 启动之前需要执行link.sh
+# !!!!!!在ubunut上执行COPY失败，windows上的git bash执行成功。
+########################################
+
 
 export DOCKER_NAME=skyreels-v1
 export DOCKER_BASE_IMAGE=${DOCKER_NAME}-base:latest
