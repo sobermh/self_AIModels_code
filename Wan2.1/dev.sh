07b76198867490513934877d909d2c298954b723
sobermh <409788696@qq.com>
2025-04-21 10:39:11 +0800

    feat:add wan2.1

---
+++ Wan2.1/dev.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+########################################
+# 构建docker镜像并启动容器服务
+########################################
+
+
+
+source env.sh
+
+# ln -s ../models/SkyReels-V1 ./ # 不能用软链接，不然docker COPY的话，路径会找到源文件路径
+cp -rf ../models/Wan2.1 ./
+
+docker compose --profile base --profile dev build  && docker compose --profile base --profile dev up -d
---
+++ Wan2.1/env.sh
@@ -0,0 +1,4 @@
+export AUTHOR=sobermh
+export DOCKER_NAME=wan2.1
+export PROJECT_NAME=Wan2.1
+export PROJECT_PATH=./${PROJECT_NAME}
